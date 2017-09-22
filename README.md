100bb4f756dc39a0d1d642c3bef906e8e9692bdd
Nolan Neustaeter <nolan@flexdealer.com>
2017-09-22 13:44:33 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+SearchLight
+===========
+
+Simple localStorage interface with timestamp based filtering and management
+
+Installation
+------------
+
+**NPM**
+
+    npm install johnny-cache
+
+**CDN**
+
+    <script src="https://unpkg.com/johnny-cache"></script>
+
+
+Usage
+-----
+
+_Note: 
+
+**ES6 Style Import**
+
+    import JohnnyCache from 'johnny-cache';
+    let cache = new JohnnyCache()
+    cache.set('stage-clothes', { color: black, coatLength: 'long' })
+    console.log(cache.get('stage-clothes'))
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp')
+var babili = require('gulp-babili')
+var rename = require('gulp-rename')
+var standard = require('gulp-standard')
+
+var script = 'src/johnny-cache.js'
+var dest = 'dist/'
+
+gulp.task(
+  'standard',
+  function () {
+    return gulp
+    .src(script)
+    .pipe(standard())
+    .pipe(
+      standard.reporter(
+        'default',
+        {
+          breakOnError: true,
+          quiet: true
+        }
+      )
+    )
+  }
+)
+
+gulp.task(
+  'js',
+  function () {
+    return gulp
+    .src(script)
+    .pipe(gulp.dest(dest))
+    .pipe(
+      babili({
+        mangle: {
+          keepClassNames: false
+        }
+      })
+    )
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(dest))
+})
+
+gulp.task('default', [ 'standard', 'js' ])
